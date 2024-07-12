32c73b1f5cc4d8fd2ee76a425153443b69e68e40
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-13 01:47:53 +0200

    make group json serializable and equatable

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # split_eth_flutter
 
 A new Flutter project.
+
+## Code Gen
+```
+dart run build_runner build
+dart run build_runner watch
+```
--- lib/models/group.dart
+++ lib/models/group.dart
@@ -1,7 +1,19 @@
-class Group {
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'group.g.dart';
+
+@JsonSerializable()
+class Group extends Equatable {
   const Group({
     required this.id,
   });
 
   final String id;
+
+  @override
+  List<Object> get props => [id];
+
+  factory Group.fromJson(Map<String, dynamic> json) => _$GroupFromJson(json);
+  Map<String, dynamic> toJson() => _$GroupToJson(this);
 }
