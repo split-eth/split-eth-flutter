b7fb1b037fdd79a836cd3564fade2e0af60ae096
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-13 01:32:51 +0200

    WIP: add group repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:split_eth_flutter/router.dart';
 
+import 'repos/local_group_repo.dart';
+
 void main() {
+  // TODO shared prefs
+  GetIt.I.registerSingleton(LocalGroupRepo());
   runApp(const MainApp());
 }
 
@@ -10,8 +15,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
+    return FutureBuilder(
+      future: GetIt.I.allReady(),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return MaterialApp.router(
+          routerConfig: router,
+        );
+      },
     );
   }
 }
---
+++ lib/repos/local_group_repo.dart
@@ -0,0 +1,11 @@
+import 'package:split_eth_flutter/models/group.dart';
+
+class LocalGroupRepo {
+  void addGroup(Group group) {
+    // TODO to local storage
+  }
+
+  // Group getGroupById(String id) {
+  //   // TODO from local storage
+  // }
+}
